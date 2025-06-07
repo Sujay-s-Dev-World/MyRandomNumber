3169144f3abfcfd86cf90b983485836f78356840
Inu(フェニックス) <dasujoy1999@gmail.com>
2025-06-07 20:20:12 +0530

    initial commit

---
+++ Controllers/RandomNumberController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRandomNumber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumberController : ControllerBase
+    {
+        private static readonly Random _random = new Random();
+
+        [HttpGet]
+        public ActionResult<int> GetRandomNumber()
+        {
+            return Ok(_random.Next(1,10));
+        }
+    }
+}
